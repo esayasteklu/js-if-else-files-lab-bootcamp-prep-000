8faf61231d5885f4f34776a017808ec6055ba6d9
Esayas Gebre <dave183278@gmail.com>
2018-05-16 11:17:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-var name1 = "susan"
+var name = "Joe"
 var height = "74"
-
-
+ 
 // Don't worry about this
-module.exports = { name1, height
-}
-
-if (name1 === "Susan") {
-    console.log("The name is correct")
-} else {
-    console.log("Expected the name to be Susan")
+module.exports = { name, height
 }
